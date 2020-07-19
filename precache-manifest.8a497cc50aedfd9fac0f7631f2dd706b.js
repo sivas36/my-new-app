9875a865a5d9fa1d3b9d35c7728dfe98c1217565
sivas36 <67056444+sivas36@users.noreply.github.com>
2020-07-19 17:44:51 +0530

    Updates

---
+++ precache-manifest.8a497cc50aedfd9fac0f7631f2dd706b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4084a5724eb4fa80a26405ad73989c4b",
+    "url": "/my-new-app-2/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/my-new-app-2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e144fd486dc37dc62a23",
+    "url": "/my-new-app-2/static/css/main.fb0a740a.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/my-new-app-2/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-new-app-2/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e144fd486dc37dc62a23",
+    "url": "/my-new-app-2/static/js/main.9512b198.chunk.js"
+  },
+  {
+    "revision": "59593586228cb4b6aa76",
+    "url": "/my-new-app-2/static/js/runtime-main.e6973e3c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/my-new-app-2/static/media/SEGA.82e09f78.TTF"
+  }
+]);
